8c341b21a2277694fbbaa3d2284b37bfc5285c16
panferov54 <65544202+panferov54@users.noreply.github.com>
2021-04-21 11:48:52 +0700

    add server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const jsonServer=require('json-server');
+const server=jsonServer.create();
+const router=jsonServer.router('./public/db2.json');
+const middlewares = jsonServer.defaults({
+    static:'./build',
+});
+const PORT =process.env.PORT||3001;
+server.use(middlewares);
+server.use(router);
+
+server.listen (PORT,()=>{
+    console.log('server is run')
+})
